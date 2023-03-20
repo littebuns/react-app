919619b7b4f728c3ae8b143f717bcc3c3b376403
xxb <769289019@qq.com>
2023-03-20 18:27:22 +0800

    补充提交

--- src/package/dataGrid/demon.js
+++ src/package/dataGrid/demon.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -11,36 +11,21 @@ const testRowData = [
 ];
 
 function DataGridDemon() {
-  const gridRef = useRef(); // Optional - for accessing Grid's API
-
-  const [columnDefs] = useState([
-    { field: "make", filter: true },
-    { field: "model", filter: true },
-    { field: "price" },
-  ]);
+  const columnDefs = [
+    { field: 'athlete' },
+    // Using dot notation to access nested property
+    { field: 'medals.gold', headerName: 'Gold' },
+  ];
 
   const [rowData, setRowData] = useState();
   useEffect(() => {
     setRowData(testRowData);
   }, []);
 
-  const buttonListener = useCallback((e) => {
-    console.log(1);
-    gridRef.current.api.deselectAll();
-  }, []);
-
   return (
-    <>
-      <button onClick={buttonListener}>Push Me</button>
-
-      <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-        <AgGridReact
-          rowData={rowData}
-          ref={gridRef}
-          columnDefs={columnDefs}
-        ></AgGridReact>
-      </div>
-    </>
+    <div className="ag-theme-alpine" style={{ height: 400, width: "100%" }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    </div>
   );
 }
 
